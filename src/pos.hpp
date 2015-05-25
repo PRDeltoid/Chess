3b7ae16b04acb28e460ee2c19a6bc05b1a5b41b9
Taylor <tbrit10@gmail.com>
2015-05-24 19:23:17 -0700

    Added Pos comparison overload
    Added the ability to use == to compare two Pos's.

--- src/pos.hpp
+++ src/pos.hpp
@@ -11,6 +11,9 @@ struct Pos {
         void set_pos(int x, int y) { x_=x; y_=y; }
         int x_,
             y_;
+        bool operator==(const Pos& pos) { 
+            return (this->x_ == pos.x_) && (this->y_ == pos.y_);
+        }
 };
 
 #endif
